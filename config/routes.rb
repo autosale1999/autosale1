169661e8ff0a644739cd2f899630066179739e81
kevyn911 <advancerp13@gmail.com>
2017-12-20 13:59:32 +0200

    Fixed design of catalog

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
 
-  get 'photos/index'
+	get 'photos/index'
 
-  get 'post/new'
+	get 'post/new'
 
-  get 'post/index'
+	get 'post/index'
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  root 'pages#index'
+	devise_for :admin_users, ActiveAdmin::Devise.config
+	ActiveAdmin.routes(self)
+	root 'pages#index'
 
-  resources :pages
+	resources :pages
 
-  resources :photos
+	resources :photos
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	devise_for :users
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
